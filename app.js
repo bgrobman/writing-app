3180a8f279cf9a515dddbf7e6ed24a8fa8d440a0
ranan grobman <binyamin.grobman@gmail.com>
2019-01-27 18:02:36 +0200

    a lot of code

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const pageNumber = document.getElementById('input-page-number');
 const paragraph = document.getElementById('input-page-content');
 const auther = document.getElementById('writers-name');
 const comment = document.getElementById('comment');
-const paragraphButton = document.getElementById('submit-paragraph');
+// const paragraphButton = document.getElementById('submit-paragraph');
 const pageButton = document.getElementById('submit-page');
 const previewParagraph = document.getElementById('prev');
 const previewPage = document.getElementById('preview');
@@ -14,9 +14,15 @@ const prevPageTitle = document.getElementById('prev-page-title');
 const prevContent = document.getElementById('prev-content');
 const pageContent = document.getElementById('preview-content');
 let valid = true;
+let ex = true;
 let prevHtml;
 var url = `https://api.airtable.com/v0/appP3yc6hsTO1FLXd/Table%201?api_key=keyZ3s4zJ9nbnOFdZ`;
 let inputAll = [bookTitle,pageTitle, pageNumber ,auther ];
+
+function retriveData(){
+  return fetch(url)
+         .then(res => res.json())
+}
 
 function testEmpty(){
   valid = true;
@@ -26,6 +32,22 @@ function testEmpty(){
   if(valid === false){
       alert('you have not filled out all the data boxes ,therfore the data will not be sent to the database!');
   }
+}
+
+function testFull(){
+  retriveData()
+  .then((data) => {
+    ex = true;
+    data.records.map(item => {
+      if(item.fields.BookTitle === bookTitle.value && item.fields.PageNumber === pageNumber.value ){
+         valid = false;
+         ex = false;
+        }
+  });
+       if(ex === false){
+         alert('this page already excists in the database!');
+       }
+});
 }
 
 function writenData(){
@@ -39,6 +61,7 @@ function writenData(){
     }
   };
 }
+
 
 function makeElement(e,content,addAtt,attName){
    var element = document.createElement(e);
@@ -56,22 +79,24 @@ function postPage(data){
   }).then(res => res.json());
 }
 
+paragraph.addEventListener("keyup", function(e){
+      if(e.keyCode == 13){
+        previewParagraph.textContent = '';
+        var p = makeElement('p',paragraph.value,'overflow','auto');
+        previewParagraph.prepend(p);
+        previewParagraph.style.border = '2px solid red';
+          var c = makeElement('p',paragraph.value);
+         paragraph.value = '';
+         $('#preview-content').append(c);
+         $('#preview-content').hide();
+  }
+ });
 
 
-paragraphButton.addEventListener('click', function(){
-  previewParagraph.textContent = '';
-  var p = makeElement('p',paragraph.value,'overflow','auto');
-  previewParagraph.prepend(p);
-  previewParagraph.style.border = '2px solid red';
-    var c = makeElement('p',paragraph.value);
-   paragraph.value = '';
-   $('#preview-content').append(c);
-   $('#preview-content').hide();
-});
 
 
  pageButton.addEventListener('click',function(){
-   $('#preview div h2, #preview div h1, #preview div h5,#preview div h6,#preview div p').remove();
+   $('#preview div h2, #preview div h1, #preview div h5,#preview div h6,#preview div p,#preview div h4').remove();
     var m = makeElement('h1', ' Book Title: ' +  bookTitle.value);
     prevBookTitle.prepend(m);
     var pt =  makeElement('h2','Page Title:' + pageTitle.value);
@@ -85,30 +110,10 @@ paragraphButton.addEventListener('click', function(){
     $('#com').css('padding','20px');
     var html =   $('#preview-content').html();
     $('#preview-content-print').html(html);
+    testFull();
     testEmpty();
     if(valid){
     postPage(writenData());
   }
     $('#preview-content p').remove();
  });
-
-
-
-// function retriveData(){
-//   return fetch(url)
-//           .then(res => res.json())
-//           .then(res => conloe.log(res))
-//           .then( (data) => {
-//             data.map(item =>
-//               if(item.records.fields.PageContent === writenData())
-//               landing.innerHTML = item.records.fields.PageContent;
-//             )
-//           })
-// }
-
-
-
-
-
-
-//mm
